dc39cf278d759d59e3eb669558a13728f13ef0a5
Nghia Nguyen Trong <nghiant@dtsmart.vn>
2024-09-17 17:36:37 +0700

    feat: add sass + tailwind css

--- components/Page/Home/HomeHero.vue
+++ components/Page/Home/HomeHero.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
       <h2>Hi nuxt</h2>
-      <p>this is section hero of the home page</p>
+      <p class=" flex bg-black text-white ">this is section hero of the home page</p>
   </div>
 </template>
