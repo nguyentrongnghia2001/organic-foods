17dbca36e027dccb288955583d21b5fd387ec0f4
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2024-09-18 23:07:28 +0700

    style: config basic

--- app.vue
+++ app.vue
@@ -1,9 +1,10 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="min-h-screen">
+    <NuxtLayout>
+      <NuxtLoadingIndicator />
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
 <script setup lang="ts">
-// You might choose this based on an API call or logged-in status
-const layout = "default";
 </script>
