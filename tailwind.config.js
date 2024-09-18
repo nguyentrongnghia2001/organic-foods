18752bc6050f7bf4f8a5045e6290fd0e0020cede
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2024-09-18 23:07:46 +0700

    feat: config tailwind color

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        'primary': 'var(--color-primary)',
+        'secondary': 'var(--color-primary)',
+        'danger': 'var(--color-danger)',
+        'success': 'var(--color-success)',
+        'dark': 'var(--color-dark)',
+      }
+    },
+  }
