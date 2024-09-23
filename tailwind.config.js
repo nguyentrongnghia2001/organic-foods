3fff07f7b4066a7e47c1a6059f23c5d363d406d2
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2024-09-24 00:09:41 +0700

    update: config tailwind base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      colors: {
-        'primary': 'var(--color-primary)',
-        'secondary': 'var(--color-primary)',
-        'danger': 'var(--color-danger)',
-        'success': 'var(--color-success)',
-        'dark': 'var(--color-dark)',
-      }
-    },
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': 'var(--color-primary)',
+      'secondary': 'var(--color-primary)',
+      'danger': 'var(--color-danger)',
+      'success': 'var(--color-success)',
+      'dark': 'var(--color-dark)',
+      'gray': 'var(--color-gray)',
+    }
+  },
+}
